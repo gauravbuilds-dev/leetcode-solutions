9cb44d97174c5a7a8930f1d3ceb36dba4a66c437
Gaurav Kumar <gaurav9572233776@gmail.com>
2026-09-24 14:47:54 +0530

    Time: 0 ms (100%), Space: 92.4 MB (61.06%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        vector<int> prefix(n);
+        prefix[0] = arr[0];
+
+        // Build prefix XOR array
+        for (int i = 1; i < n; i++)
+            prefix[i] = prefix[i - 1] ^ arr[i];
+
+        vector<int> result;
+        for (auto& q : queries) {
+            int L = q[0], R = q[1];
+            if (L == 0)
+                result.push_back(prefix[R]);
+            else
+                result.push_back(prefix[R] ^ prefix[L - 1]);
+        }
+
+        return result;
+    }
+};
